d3dc6d9bad4ab96aa48d8acd96d332e71a37d909
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-27 21:43:27 +0200

    Update ThemeSwitcher.jsx

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,22 +1,22 @@
-// import { BsSun, BsMoon } from 'react-icons/bs';
-// import { useDispatch, useSelector } from 'react-redux';
+import { BsSun, BsMoon } from 'react-icons/bs';
+import { useDispatch, useSelector } from 'react-redux';
 
-// import { toggleThemeTitle } from 'redux/global/globalSlice';
-// import { selectThemeTitle } from 'redux/global/selectors';
+import { toggleThemeTitle } from 'redux/global/globalSlice';
+import { selectThemeTitle } from 'redux/global/selectors';
 
-// import { Switcher } from './ThemeSwitcher.styled';
+import { Switcher } from './ThemeSwitcher.styled';
 
-// export function ThemeSwitcher() {
-//   const themeTitle = useSelector(selectThemeTitle);
-//   const dispatch = useDispatch();
+export function ThemeSwitcher() {
+  const themeTitle = useSelector(selectThemeTitle);
+  const dispatch = useDispatch();
 
-//   const toggleTheme = () => {
-//     dispatch(toggleThemeTitle());
-//   };
+  const toggleTheme = () => {
+    dispatch(toggleThemeTitle());
+  };
 
-//   return (
-//     <Switcher type="button" onClick={toggleTheme}>
-//       {themeTitle === 'light' ? <BsSun size={30} /> : <BsMoon size={30} />}
-//     </Switcher>
-//   );
-// }
+  return (
+    <Switcher type="button" onClick={toggleTheme}>
+      {themeTitle === 'light' ? <BsSun size={30} /> : <BsMoon size={30} />}
+    </Switcher>
+  );
+}
