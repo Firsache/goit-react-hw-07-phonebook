2641c8bde2e8d000782a6d3d36842ffe2b4af38b
Firsova Liubov <firsova.lyub@gmail.com>
2023-02-28 11:19:25 +0200

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import { createSlice, nanoid } from '@reduxjs/toolkit';
 import { getContacts, addContacts, delContacts } from 'redux/operations';
 
 const initialContactsState = {
@@ -14,17 +13,6 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState: initialContactsState,
   reducers: {
-    // addContact: {
-    //   prepare: newContact => {
-    //     return { payload: { ...newContact, id: nanoid(6) } };
-    //   },
-    //   reducer: (state, { payload }) => {
-    //     state.contacts = [payload, ...state.contacts];
-    //   },
-    // },
-    // deleteContact(state, { payload }) {
-    //   state.contacts = state.contacts.filter(c => c.id !== payload);
-    // },
     setFilteredName(state, { payload }) {
       state.filteredName = payload;
     },
